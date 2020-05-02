f07319075aeda046affc1e9f4f1ce1235273b58c
成镇宇 <cheng-zy17@mails.tsinghua.edu.cn>
2020-05-02 18:08:08 +0900

    question_1: added input generator

--- Question_1/plaintext_generator.py
+++
@@ -1,9 +0,0 @@
-from string import ascii_letters, punctuation, digits
-from random import choice
-
-string_format = ascii_letters + punctuation + digits
-generated_string = "".join(choice(string_format) for x in range(int(16000)))
-
-f = open("./plaintext.txt", 'rb+')
-f.write(generated_string.encode())
-f.close()
